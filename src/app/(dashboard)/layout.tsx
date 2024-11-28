0147bb2a8280073327127482d039e4ffe115d550
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-11-28 15:49:38 +0200

    created menu component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Menu from "@/components/Menu";
 
 export default function DashboardLayout({
     children,
@@ -7,13 +8,14 @@ export default function DashboardLayout({
   }>) {
     return (
       <div className="h-screen flex">
-        <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+        <div className="w-1/6 md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
         <img src="/logo.png" alt="logo" width={32} height={32} ></img>
         <span className="hidden lg:block ">Ami School</span>
         </Link>
+        <Menu/>
         </div>
-        <div className="w-5/6 md:w-[92%] lg:w-[84%] md:w-[86%] bg-blue-200"></div>
+        <div className="w-5/6 md:w-[92%] lg:w-[84%] md:w-[86%]"></div>
       </div>
     )
   }
