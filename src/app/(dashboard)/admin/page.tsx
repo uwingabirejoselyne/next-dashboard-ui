8283872054374abdbaffa5ebc57d17df3df822ae
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-12-02 10:27:48 +0200

    completed usecard component

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,14 @@
+import UserCard from "@/components/UserCard"
 const AdminPage = () => {
   return (
     <div className='flex flex-col md:flex-row p-4 gap-4'>
-      <div className="w-full lg:w-2/3">l
+      <div className="w-full lg:w-2/3">
+      <div className="flex justify-between gap-4 flex-wrap lg:flex">
+        <UserCard type="Student"/>
+        <UserCard type="Teacher"/>
+        <UserCard type="Parent"/>
+        <UserCard type="Staff"/>
+      </div>
       </div>
       <div className="w-full lg:w-2/3">r</div>
     </div>
