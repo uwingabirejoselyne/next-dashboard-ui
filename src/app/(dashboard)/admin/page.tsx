851df1f332e08db18912a1a80c8301b965c9e4a1
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-12-05 10:41:52 +0200

    completed finance chart component

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,24 +1,28 @@
 import AttendenceChart from "@/components/AttendenceChart"
 import CountChart from "@/components/CountChart"
+import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 const AdminPage = () => {
   return (
     <div className='flex flex-col md:flex-row p-4 gap-4'>
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-      <div className="flex justify-between gap-4 flex-wrap lg:flex">
-        <UserCard type="Student"/>
-        <UserCard type="Teacher"/>
-        <UserCard type="Parent"/>
-        <UserCard type="Staff"/>
-      </div>
-      <div className="flex flex-col lg:flex-row gap-2">
-        <div className="w-full lg:w-1/3 h-[450px]">
-          <CountChart/>
+        <div className="flex justify-between gap-4 flex-wrap lg:flex">
+          <UserCard type="Student" />
+          <UserCard type="Teacher" />
+          <UserCard type="Parent" />
+          <UserCard type="Staff" />
         </div>
-        <div className="w-full lg:w-2/3 h-[450px]">
-          <AttendenceChart/>
+        <div className="flex flex-col lg:flex-row gap-2">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendenceChart />
+          </div>
         </div>
-      </div>
+        <div className="w-full h-[500px]">
+          <FinanceChart/>
+        </div>
       </div>
     </div>
   )
