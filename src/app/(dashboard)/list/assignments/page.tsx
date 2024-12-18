73f0191b36a3b88a0df3031465016fd67af1a838
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-12-18 13:33:05 +0200

    created assignements list page

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 import Pagination from '@/components/Pagination'
 import Table from '@/components/Table'
 import Link from 'next/link'
-import {  examsData, lessonsData, role, } from '@/lib/data'
+import {  assignmentsData, examsData, lessonsData, role, } from '@/lib/data'
 
-type Exam = {
+type  Assignment = {
     id: string;
     subject: string;
     class: number;
     teacher: number;
-    date:string
+    dueDate:string
 }
 
 const columns = [
@@ -31,8 +31,8 @@ const columns = [
         className: "hidden md:table-cell"
     },
     {
-        header: "Date",
-        accessor: "date",
+        header: "Due Date",
+        accessor: "dueDate",
         className: "hidden md:table-cell"
     },
     {
@@ -41,8 +41,8 @@ const columns = [
         className: "hidden md:table-cell"
     }
 ]
-const ExamListPage = () => {
-    const renderRow = (item: Exam) => (
+const AssignmentListPage = () => {
+    const renderRow = (item: Assignment) => (
         <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight'>
             <td className='flex items-center gap-4 p-4'>
                 {item.subject}
@@ -54,7 +54,7 @@ const ExamListPage = () => {
                 {item.teacher}
             </td>
             <td className='hidden md:table-cell'>
-                {item.date}
+                {item.dueDate}
             </td>
             <td>
                 <div className="flex items-center gap-2">
@@ -75,7 +75,7 @@ const ExamListPage = () => {
     return (
         <div className='bg-white rounded-md p-4 flex-1 m-4 mt-0'>
             <div className='flex items-center justify-between'>
-                <h1 className='hidden md:block font:semi-bold text-lg'>All Exams</h1>
+                <h1 className='hidden md:block font:semi-bold text-lg'>All Assignments</h1>
                 <div className='flex flex-col md:flex-row items-center gap-4  w-full md:w-auto'>
                     <TableSearch />
                     <div className='flex items-center gap-4 self-end'>
@@ -93,11 +93,11 @@ const ExamListPage = () => {
                     </div>
                 </div>
             </div>
-            <Table columns={columns} renderRow={renderRow} data={examsData} />
+            <Table columns={columns} renderRow={renderRow} data={assignmentsData} />
             <Pagination />
         </div>
     )
 
 }
 
-export default ExamListPage
+export default AssignmentListPage
