731d18cd3357dca500ecad1790691dd35f77b32c
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-01-06 11:02:42 +0200

    react hook form package installation

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -17,7 +17,7 @@ const FormModal = ({table,type,data,id}:{
      (
       <form action="" className='p-4 flex flex-col gap-4'>
       <span className='text-center font-medium'>All data will be lost, Are you sure you want to delete this {table}?</span>
-      <button className='bg-red-700 text-white py-2 px-4 rounded-md border-none'>Delete</button>
+      <button className='bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center'>Delete</button>
     </form>
      ):("create or update form")
     }
