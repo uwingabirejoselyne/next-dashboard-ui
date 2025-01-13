b980e72f90c8173428d2298feda22924303f8010
uwingabirejoselyne <uwingajoselyne@gmail.com>
2025-01-13 10:37:41 +0200

    created input field component

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 import {  } from 'recharts';
 import { z } from 'zod';
+import InputField from '../InputField';
 
 
 const schema = z.object({
@@ -19,6 +20,7 @@ const schema = z.object({
   sex: z.enum(["male", "female"], { message: "Sex is required and must be 'male' or 'female'!" }),
   img:z.instanceof(File, {message:"Image is requires"})
 });
+type inputs = z.infer<typeof schema>
 const TeacherForm = ({
     type,data}:{
         type:"create"|"update";
@@ -29,7 +31,7 @@ const TeacherForm = ({
         register,
         handleSubmit,
         formState: { errors },
-      } = useForm({
+      } = useForm<inputs>({
         resolver: zodResolver(schema),
       });
   const onSubmit = handleSubmit(data=>{
@@ -40,11 +42,13 @@ const TeacherForm = ({
     <form className='flex flex-col gap-8' onSubmit={onSubmit}>
       <h1 className='text-xl font-semibold'>Create a new teacher</h1>
       <span className='text-xs text-gray-400 font-medium'>Authentication Information</span>
-      <div className='flex flex-col gap-2 w-full md:w-1/4'>
-      <label className='text-xs text-gray-500'>Username</label>
-      <input type="text" {...register("username")} className='ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full'/>
-      {errors.username?.message && <p className='text-xs text-red-400'>{errors.username?.message?.toString()}</p>}
-      </div>
+      <InputField
+      label="Username"
+      name="username"
+      defaultValue={data?.username}
+      register={register}
+      error={errors.username}
+      />
       <span className='text-xs text-gray-400 font-medium'>Personal Info</span>
       <button className='bg-blue-400 text-white p-2 rounded-md'>{type ==="create"?"Create":"Update"}</button>
     </form>
